e18f3d9ba7dbf5e6a9577d26a3dc2cc431ff316b
franzele <muller.francois.7@gmail.com>
2023-11-05 02:39:32 +0100

    [FMU] NEW: initialisation of the naive bayes file (nothing was done yet because j'ai été pris de cours par l'autre programme create_centered_dataset de ces morts il est 02h39 du matin à l'aide)

---
+++ scripts/points_coord_by_emotions.py
@@ -0,0 +1,8 @@
+"""
+This script is used to determine the mean coordinates of all points
+for each emotions
+"""
+
+# CONSTANTES (DON'T CHANGE IT)
+FACE_DIR    = r"CK+_lands/CK+/"
+FACE_FILE   = "omlands.csv"
