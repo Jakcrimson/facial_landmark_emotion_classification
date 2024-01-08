95b33bd4311841cd71a564549d1a421efa1debd8
Jakcrimson <pierre.lague@gmail.com>
2024-01-08 14:27:25 +0100

    temp stash for final pull

--- scripts/save_face_img.py
+++ scripts/save_face_img.py
@@ -1,3 +1,5 @@
+# this script is to be executed using the CNN_methode_elegante notebook, not on it's own, path will not match.
+
 from tqdm import tqdm
 from sklearn.model_selection import train_test_split
 from PIL import Image
@@ -7,9 +9,9 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-ORIGIN_DATASET  = "CK+_lands/CK+_centered"
-EMOTIONS_INDEX  = "CK+_lands/CK+/emotion.csv"
-IMAGES_DIR      = "CK+_lands/images"
+ORIGIN_DATASET  = "../CK+_lands/CK+_centered"
+EMOTIONS_INDEX  = "../CK+_lands/CK+/emotion.csv"
+IMAGES_DIR      = "../CK+_lands/images"
 EMOTIONS        = {
     1: "happy",
     2: "fear",
@@ -38,49 +40,63 @@ for split_name in ["train", "val", "test"]:
 
 # retrieve the emotions dataset
 emotions = pd.read_csv(EMOTIONS_INDEX, header=0, delimiter=";")
-print(emotions)
 # creation of the dataset split (here 70-15-15)
 dataset_split = np.arange(len(emotions))
-X_train, X_test = train_test_split(dataset_split, stratify=emotions["emotion"], test_size=15) #stratify for proportionate class balance
-X_train, X_val  = train_test_split(X_train, stratify=emotions.iloc[X_train]["emotion"], test_size=15) #stratify for proportionate class balance
+X_train, X_test = train_test_split(dataset_split, stratify=emotions["emotion"], test_size=10) #stratify for proportionate class balance
+X_train, X_val  = train_test_split(X_train, stratify=emotions.iloc[X_train]["emotion"], test_size=20) #stratify for proportionate class balance
 
 def colonne_zone(zone):
-    match zone:
-        case 0:         # joue gauche
-            return [str(2*x+y) for x in range(9) for y in range(2)]
-        case 1:         # joue droite
-            return [str(2*x+y) for x in range(9, 17) for y in range(2)]
-        case 2:         # sourcil gauche
-            return [str(2*x+y) for x in range(17, 22) for y in range(2)]
-        case 3:         # sourcil droite
-            return [str(2*x+y) for x in range(22, 27) for y in range(2)]
-        case 4:         # nez
-            return [str(2*x+y) for x in range(27, 36) for y in range(2)]
-        case 5:         # oeil gauche
-            return [str(2*x+y) for x in range(36, 42) for y in range(2)]
-        case 6:         # oeil droite
-            return [str(2*x+y) for x in range(42, 48) for y in range(2)]
-        case 7:         # lèvre supéreure
-            return [str(2*x+y) for x in range(48, 56) for y in range(2)] + \
-                    [str(2*x+y) for x in range(61, 65) for y in range(2)]
-        case 8:         # lèvre inférieure
-            return [str(2*x+y) for x in range(55, 61) for y in range(2)] + \
-                    [str(2*x+y) for x in range(64, 68) for y in range(2)]
-        
+    if zone == 0:  # joue gauche
+        return [str(2*x+y) for x in range(9) for y in range(2)]
+    elif zone == 1:  # joue droite
+        return [str(2*x+y) for x in range(9, 17) for y in range(2)]
+    elif zone == 2:  # sourcil gauche
+        return [str(2*x+y) for x in range(17, 22) for y in range(2)]
+    elif zone == 3:  # sourcil droite
+        return [str(2*x+y) for x in range(22, 27) for y in range(2)]
+    elif zone == 4:  # nez
+        return [str(2*x+y) for x in range(27, 36) for y in range(2)]
+    elif zone == 5:  # oeil gauche
+        return [str(2*x+y) for x in range(36, 42) for y in range(2)]
+    elif zone == 6:  # oeil droite
+        return [str(2*x+y) for x in range(42, 48) for y in range(2)]
+    elif zone == 7:  # lèvre supérieure
+        return [str(2*x+y) for x in range(48, 56) for y in range(2)] + \
+               [str(2*x+y) for x in range(61, 65) for y in range(2)]
+    elif zone == 8:  # lèvre inférieure
+        return [str(2*x+y) for x in range(55, 61) for y in range(2)] + \
+               [str(2*x+y) for x in range(64, 68) for y in range(2)]
 
+
+def create_faces(color_mode="bw"):
 # create all the faces 
-for emo_id in tqdm(emotions.index[4:]):
-    tmp_subject, tmp_file, tmp_emotion = emotions.loc[emo_id, :]
-    tmp_points = pd.read_csv(os.path.join(ORIGIN_DATASET, tmp_subject, "omlands.csv"), header=None, delimiter=";")
-    tmp_points = tmp_points.where(tmp_points.iloc[:, 0] == tmp_file).dropna()
-    tmp_points = tmp_points.iloc[-1, 1:]
+    if color_mode=="bw":
+        for emo_id in tqdm(emotions.index[4:]):
+            tmp_subject, tmp_file, tmp_emotion = emotions.loc[emo_id, :]
+            tmp_points = pd.read_csv(os.path.join(ORIGIN_DATASET, tmp_subject, "omlands.csv"), header=None, delimiter=";")
+            tmp_points = tmp_points.where(tmp_points.iloc[:, 0] == tmp_file).dropna()
+            tmp_points = tmp_points.iloc[-1, 1:]
 
-    direction = "train" if emo_id in X_train else "val" if emo_id in X_val else "test"
+            direction = "train" if emo_id in X_train else "val" if emo_id in X_val else "test"
 
-    # saving the faces
-    # plt.scatter(tmp_points[::2], -tmp_points[1::2], c="black")
-    for i in range(9):
-        plt.scatter(tmp_points.iloc[colonne_zone(i)[::2]], -tmp_points.iloc[colonne_zone(i)[1::2]])
-    plt.axis("off")
-    plt.savefig(os.path.join(IMAGES_DIR, direction, EMOTIONS[tmp_emotion], f"{tmp_subject}.png"), bbox_inches='tight')
-    plt.clf()
+            # saving the faces
+            plt.scatter(tmp_points[::2], -tmp_points[1::2], c="black")
+            plt.axis("off")
+            plt.savefig(os.path.join(IMAGES_DIR, direction, EMOTIONS[tmp_emotion], f"{tmp_subject}.png"), bbox_inches='tight')
+            plt.clf()
+
+    if color_mode=="rgb":
+        for emo_id in tqdm(emotions.index[4:]):
+            tmp_subject, tmp_file, tmp_emotion = emotions.loc[emo_id, :]
+            tmp_points = pd.read_csv(os.path.join(ORIGIN_DATASET, tmp_subject, "omlands.csv"), header=None, delimiter=";")
+            tmp_points = tmp_points.where(tmp_points.iloc[:, 0] == tmp_file).dropna()
+            tmp_points = tmp_points.iloc[-1, 1:]
+
+            direction = "train" if emo_id in X_train else "val" if emo_id in X_val else "test"
+
+            # saving the faces
+            for i in range(9):
+                plt.scatter(tmp_points.iloc[colonne_zone(i)[::2]], -tmp_points.iloc[colonne_zone(i)[1::2]])
+            plt.axis("off")
+            plt.savefig(os.path.join(IMAGES_DIR, direction, EMOTIONS[tmp_emotion], f"{tmp_subject}.png"), bbox_inches='tight')
+            plt.clf()
